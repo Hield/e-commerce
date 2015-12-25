afa9b5817ef0fa47bf3276e542b15db5ef43c1e7
Lieu Vu <dinhlieuvu@gmail.com>
2015-12-25 17:01:39 +0200

    Them code trong file about.php Them file map.js

--- views/pages/about.php
+++ views/pages/about.php
@@ -4,4 +4,67 @@
 		<li><a href="<?php echo $_SERVER['PHP_SELF'];?>">Home</a></li>
 		<li class="active">About</li>	
 	</ol>
+	
+		<!-- About us content -->
+
+	<div>
+		<div class="container-fluid bg-1 text-center">
+  			<div class="row">
+  				<div class="col-sm-8">
+	  				<h1>ABOUT COMPANY</h1>
+	  				<p>Established in 2015, THL is one of fastest growing international company which provides the customers effective and innovative
+	  			   solutions to online trading.</p>
+	  			</div>
+	  			<div class="col-sm-4">
+	      			<span class="glyphicon glyphicon-signal logo1"></span>
+	    		</div>
+  			</div>
+		</div> <!--end bg-1 -->
+		
+		<div class="container-fluid bg-2">
+  			<div class="row">
+  				<div class="col-sm-5 vcenter">
+      				<span class="glyphicon glyphicon-globe logo1"></span> 
+    			</div>
+    			<div class="col-sm-7">
+      				<h1>Our Values</h1>
+      				<div class="row">
+      					<div class="col-sm-4">
+		      				<h4><strong>MISSION:</strong></h4> 
+		      			</div>
+      					<div class="col-sm-8">
+      						<h4>To provide the customers with the best service and high-quality product</h4>
+      					</div>
+      				</div>
+      				<div class="row">
+      					<div class="col-sm-4">
+		      				<h4><strong>VISION:</strong></h4> 
+		      			</div>
+      					<div class="col-sm-8">
+      						<h4> To create a eco-friendly and self-intelligent e-commerece environment</h4>
+      					</div>
+      				</div>
+    			</div>
+  			</div>
+  		</div> <!--end bg-2 -->
+  		
+  		<div class="container-fluid bg-3 text-center">
+  			<h1>Proudly Partnering With</h1>
+  			<div class="row partnerlogo">
+  				<div class="col-sm-4">
+  					<span class="glyphicon glyphicon-send logo2"> Carletto</span>
+  				</div>
+  				<div class="col-sm-4">
+  					<span id="bold" class="glyphicon glyphicon-piggy-bank logo2"> SPBank</span>
+  				</div>
+  				<div class="col-sm-4">
+  					<span class="glyphicon glyphicon-flag logo2"> LEADER</span>
+  				</div>
+  			</div> <!--end bg-3 -->
+
+  		</div> 
+
+	</div>
+	
+	
 </div>

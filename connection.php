8d7689179907a577ff83a9468e3bb6a4c582fd62
Trung Hiếu Lê <hieult1409@gmail.com>
2015-12-25 18:42:29 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -9,7 +9,7 @@
 		public static function getInstance(){
 			if (!isset(self::$instance)){
 				$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-				self::$instance = new PDO('mysql:host=localhost;dbname=trunghil', 'root', '', $pdo_options);
+				self::$instance = new PDO('mysql:host=mysql.metropolia.fi;dbname=trunghil', 'trunghil', '123456', $pdo_options);
 			}
 			return self::$instance;
 		}

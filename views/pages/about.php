ba07cc9693c4fb87bae9ae4626999bab4bfb7a34
Lieu Vu <dinhlieuvu@gmail.com>
2015-12-25 16:49:36 +0200

    no message

--- views/pages/about.php
+++ views/pages/about.php
@@ -4,16 +4,4 @@
 		<li><a href="<?php echo $_SERVER['PHP_SELF'];?>">Home</a></li>
 		<li class="active">About</li>	
 	</ol>
-		<div class="row">
-		<div class="col-xs-6">
-			<p style="font-size: 20px; font-weight: bold;">THL OY</p>
-			<p>Address: Vanha maantie 6 02650 Leppävaara, Espoo</p>
-			<p>Fax: 044-888 8888</p>
-			<p>Tel: 044-999 9999</p>
-			<p>Email: info@thl.fi</p>
-		</div>
-		<div class="col-xs-6">
-			<div id="map-container"></div>
-		</div>
-	</div>
 </div>

bc26296de06a882a5889e0777ac333e3e010ce52
Trung Hiếu Lê <hieult1409@gmail.com>
2015-12-25 18:43:02 +0200

    Update prepare.php

--- assets/components/prepare.php
+++ assets/components/prepare.php
@@ -6,9 +6,6 @@
 	if (!isset($_SESSION['permission'])){
 		$_SESSION['permission'] = "user";
 	}
-	ini_set('display_startup_errors',1);
-	ini_set('display_errors',1);
-	error_reporting(-1);
 	function getLink($controller, $action){
 		if ($controller == "pages" && $action == "home"){
 			echo $_SERVER['PHP_SELF'];

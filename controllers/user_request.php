0b7587b1e4424c7aedb19aa3af75d7ad6ba37720
Lieu Vu <dinhlieuvu@gmail.com>
2016-01-05 15:42:13 +0200

    Replace file user_get.php by uer_request.php

--- controllers/user_request.php
+++ controllers/user_request.php
@@ -4,20 +4,18 @@
 
 	$checkUser = array("foundUserName"  => 0, "pwdMatch" => 0);
 	$databasePwd = '';
-
-	//$user = User::find($_POST['username'], $_POST['pwd']);
-	$user = User::find_userName($_POST['username']);
+	$username = $_POST['username'];
 	$inputPwd = $_POST['pwd'];
-	
-	 if ($user) {
-	 	$checkUser['foundUserName'] = 1;
-	 	$databasePwd = $user->pwd;
-	if($databasePwd == $inputPwd){
-	 		$checkUser['pwdMatch'] = 1;
-	 	}
-	 }
+	$user = User::find_by_username($username);
+
+	if ($user) {
+		$checkUser['foundUserName'] = 1;
+		$databasePwd = $user->pwd;
+		if($databasePwd == $inputPwd){
+			$checkUser['pwdMatch'] = 1;
+		}
+	}
 
 	echo $checkUser['foundUserName'].',';
 	echo $checkUser['pwdMatch'];
-
 ?>

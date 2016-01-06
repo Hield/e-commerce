ff9cadaf1093413eb9fbc9f9ee355380e515b61f
Le Trung Hieu <hieult1409@gmail.com>
2016-01-06 16:40:21 +0200

    Update

--- assets/components/prepare.php
+++ assets/components/prepare.php
@@ -9,6 +9,7 @@
 	ini_set('display_startup_errors',1);
 	ini_set('display_errors',1);
 	error_reporting(-1);
+	
 	function getLink($controller, $action){
 		if ($controller == "pages" && $action == "home"){
 			echo $_SERVER['PHP_SELF'];
--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -1,4 +1,4 @@
-<?php
+<?php	
 	class UsersController {
 		public function login(){
 			if (!isset($_POST['username']) || !isset($_POST['pwd'])){

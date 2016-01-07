3ec4ef8c5bde272358b4a4fbced07d6501013f5b
Lieu Vu <dinhlieuvu@gmail.com>
2016-01-07 17:09:32 +0200

    Update aboutus.php and contact.php Modify style in style.css for images in aboutus.php and contact.php

--- views/pages/about.php
+++ views/pages/about.php
@@ -7,64 +7,51 @@
 	
 		<!-- About us content -->
 
-	<div>
 		<div class="container-fluid bg-1 text-center">
-  			<div class="row">
-  				<div class="col-sm-8">
-	  				<h1>ABOUT COMPANY</h1>
-	  				<p>Established in 2015, THL is one of fastest growing international company which provides the customers effective and innovative
-	  			   solutions to online trading.</p>
-	  			</div>
-	  			<div class="col-sm-4">
-	      			<span class="glyphicon glyphicon-signal logo1"></span>
-	    		</div>
-  			</div>
+			<div class="row">
+				<div class="col-sm-4 img-author">
+					 <img src="assets/img/slider1.png">
+				</div>
+				<div class="col-sm-8">
+					<h1>HIEU LE</h1>
+					<p>Metropolia University of Applied Sciences Student</p>
+					<p>Class: 2014</p>
+					<p>Degree: Bachelor Degree</p>
+					<p>Degree Program: Information Technology</p>
+					<p>Specialization: Software Engineering</p>
+				</div>
+			</div>
 		</div> <!--end bg-1 -->
 		
 		<div class="container-fluid bg-2">
-  			<div class="row">
-  				<div class="col-sm-5 vcenter">
-      				<span class="glyphicon glyphicon-globe logo1"></span> 
-    			</div>
-    			<div class="col-sm-7">
-      				<h1>Our Values</h1>
-      				<div class="row">
-      					<div class="col-sm-4">
-		      				<h4><strong>MISSION:</strong></h4> 
-		      			</div>
-      					<div class="col-sm-8">
-      						<h4>To provide the customers with the best service and high-quality product</h4>
-      					</div>
-      				</div>
-      				<div class="row">
-      					<div class="col-sm-4">
-		      				<h4><strong>VISION:</strong></h4> 
-		      			</div>
-      					<div class="col-sm-8">
-      						<h4> To create a eco-friendly and self-intelligent e-commerece environment</h4>
-      					</div>
-      				</div>
-    			</div>
-  			</div>
-  		</div> <!--end bg-2 -->
-  		
-  		<div class="container-fluid bg-3 text-center">
-  			<h1>Proudly Partnering With</h1>
-  			<div class="row partnerlogo">
-  				<div class="col-sm-4">
-  					<span class="glyphicon glyphicon-send logo2"> Carletto</span>
-  				</div>
-  				<div class="col-sm-4">
-  					<span id="bold" class="glyphicon glyphicon-piggy-bank logo2"> SPBank</span>
-  				</div>
-  				<div class="col-sm-4">
-  					<span class="glyphicon glyphicon-flag logo2"> LEADER</span>
-  				</div>
-  			</div> <!--end bg-3 -->
-
-  		</div> 
-
-	</div>
-	
-	
-</div>
+			<div class="row">
+				<div class="col-sm-4 img-author">
+					 <img src="assets/img/author_2.jpg">
+				</div>
+				<div class="col-sm-8">
+					<h1>LIEU VU</h1>
+					<p>Metropolia University of Applied Sciences Student</p>
+					<p>Class: 2014</p>
+					<p>Degree: Bachelor Degree</p>
+					<p>Degree Program: Information Technology</p>
+					<p>Specialization: Software Engineering</p>
+				</div>
+			</div>
+		</div> <!--end bg-2 -->
+		
+		<div class="container-fluid bg-3">
+			<div class="row">
+				<div class="col-sm-4 img-author">
+					 <img src="assets/img/slider1.png">
+				</div>
+				<div class="col-sm-8">
+					<h1>TRI TRAN</h1>
+					<p>Metropolia University of Applied Sciences Student</p>
+					<p>Class: 2014</p>
+					<p>Degree: Bachelor Degree</p>
+					<p>Degree Program: Information Technology</p>
+					<p>Specialization: Software Engineering</p>
+				</div>
+			</div>
+		</div> <!--end bg-3 -->
+</div> 
--- views/pages/contact.php
+++ views/pages/contact.php
@@ -4,16 +4,46 @@
 		<li><a href="<?php echo $_SERVER['PHP_SELF'];?>">Home</a></li>
 		<li class="active">Contact</li>	
 	</ol>
+	
+	<div class="container-fluid bg-1 text-center">
 		<div class="row">
-		<div class="col-xs-6">
-			<p style="font-size: 20px; font-weight: bold;">THL OY</p>
-			<p>Address: Vanha maantie 6 02650 Leppävaara, Espoo</p>
-			<p>Fax: 044-888 8888</p>
-			<p>Tel: 044-999 9999</p>
-			<p>Email: info@thl.fi</p>
-		</div>
-		<div class="col-xs-6">
-			<div id="map-container"></div>
-		</div>
+			<div class="col-sm-4 img-author">
+				 <img src="assets/img/slider1.png">
+			</div>
+			<div class="col-sm-8">
+				<h1>HIEU LE</h1>
+				<p>Address: </p>
+				<p>Email: </p>
+				<p>Mobile: </p>
+			</div>
+		</div> <!--end bg-1 -->
 	</div>
+
+	<div class="container-fluid bg-2">
+			<div class="row">
+			<div class="col-sm-4 img-author">
+				 <img src="assets/img/author_2.jpg">
+			</div>
+			<div class="col-sm-8">
+				<h1>LIEU VU</h1>
+				<p>Address: Kääpiöidenpolku 4 B 36 00820 Helsinki</p>
+				<p>Email: dinhlieuvu@gmail.com</p>
+				<p>Mobile: +358 442 099 588</p>
+			</div>
+		</div> <!--end bg-1 -->
+		</div> <!--end bg-2 -->
+		
+		<div class="container-fluid bg-3">
+			<div class="row">
+			<div class="col-sm-4 img-author">
+				<img src="assets/img/slider1.png">
+			</div>
+			<div class="col-sm-8">
+				<h1>TRI TRAN</h1>
+				<p>Address: </p>
+				<p>Email: </p>
+				<p>Mobile: </p>
+			</div>
+		</div> <!--end bg-1 -->
+		</div> <!--end bg-3 -->
 </div>

e8260eef4d24797b5c554be77c2a87520b4593d5
Le Trung Hieu <hieult1409@gmail.com>
2016-01-09 17:41:32 +0200

    Update link to products list

--- views/pages/home.php
+++ views/pages/home.php
@@ -19,7 +19,7 @@
                         <div class="carousel-caption">
                             <h1>Welcome to THL.</h1>
                             <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.</p>
-                            <a class="btn btn-default myButton">SHOP NOW</a>
+                            <a href="index.php?controller=products&action=index" class="btn btn-default myButton">SHOP NOW</a>
                         </div>
 
                     </div>
@@ -37,7 +37,7 @@
                         <div class="carousel-caption">
                             <h1>Easy management</h1>
                             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                            <a class="btn btn-default myButton">SHOP NOW</a>
+                            <a href="index.php?controller=products&action=index" class="btn btn-default myButton">SHOP NOW</a>
                         </div>
 
                     </div>
@@ -55,7 +55,7 @@
                         <div class="carousel-caption">
                             <h1>High quality</h1>
                             <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-					 <a class="btn btn-default myButton">SHOP NOW</a>
+					        <a href="index.php?controller=products&action=index" class="btn btn-default myButton">SHOP NOW</a>
                         </div>
 
                     </div>
